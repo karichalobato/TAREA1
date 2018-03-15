8671052cf163d288585cb8856ddae10cc64026e3
UCA <edwin.morales9b@gmail.com>
2018-03-14 20:07:58 -0600

    Proyecto imprimir información

---
+++ src/tarea1/TAREA1.java
@@ -0,0 +1,19 @@
+
+package tarea1;
+
+/**
+ *
+ * @author Karicha Lobato
+ */
+public class TAREA1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola !!!!!");
+        System.out.println("Nombre: Karicha Valesska Romero Lobato");
+        System.out.println("Carnet: 00002517");
+        System.out.println("Facultad: Ingeniería y Arquitectura");
+        System.out.println("Carrera: Ingeniería Informática ciclo 01/2018");
+        
+    }
+    
+}
